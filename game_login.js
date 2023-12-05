bcb41d81bb5fcdfbd271fa49cd6c3294af032cb7
salvadorcar <143309978+salvadorcar@users.noreply.github.com>
2023-12-05 12:51:31 -0600

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,15 @@
+// Crea la función addUser()
+
+a
+
+function ____ {
+  // Obtén el valor del usuario con id player1_name_input y player2_name_input
+
+
+
+
+  // Almacena los valores en el almacenamiento local
+
+  //Asigna "game_page.html" a window.location
+}
+
